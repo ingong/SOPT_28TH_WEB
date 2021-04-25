934f2c6ffea6447f4c3ec1ce527464a231130ce7
leeinsong <insong1216@gmail.com>
2021-04-25 21:10:14 +0900

    Add: dark mode

--- 2nd_seminar/index.js
+++ 2nd_seminar/index.js
@@ -56,5 +56,25 @@ const changeTime = () => {
   }
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+  let theme = localStorage.getItem("theme");
+  if (!theme) {
+    const { matches } = window.matchMedia("(prefers-color-scheme: dark)");
+    theme = matches ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+  }
+
+  document.body.classList.toggle("dark", theme === "dark");
+  setTimeout(() => {
+    document.body.style.visibility = "visible";
+  }, 300);
+});
+
+document.querySelector(".toggle-button").onclick = (e) => {
+  const theme = localStorage.getItem("theme");
+  localStorage.setItem("theme", `${theme === "dark" ? "light" : "dark"}`);
+  document.body.classList.toggle("dark");
+};
+
 changeClock.addEventListener("click", changeTime);
 init();
