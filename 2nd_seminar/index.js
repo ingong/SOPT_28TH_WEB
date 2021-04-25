3b1aabf3e0a748a69d4a7dce602b3fdf6e334b67
leeinsong <insong1216@gmail.com>
2021-04-25 21:14:47 +0900

    Add: modal

--- 2nd_seminar/index.js
+++ 2nd_seminar/index.js
@@ -4,6 +4,8 @@ const hourhand = document.querySelector(".hour-hand");
 const clockTarget = document.querySelector(".digitalClock");
 const changeClock = document.querySelector(".changeClock");
 let hour24 = true;
+const bodyBlackout = document.querySelector(".body-blackout");
+const popupModal = document.querySelector(".popup-modal");
 
 function setAnalog() {
   const now = new Date();
@@ -78,3 +80,18 @@ document.querySelector(".toggle-button").onclick = (e) => {
 
 changeClock.addEventListener("click", changeTime);
 init();
+
+document.querySelector(".popup-trigger").addEventListener("click", function () {
+  popupModal.classList.add("is--visible");
+  bodyBlackout.classList.add("is-blacked-out");
+});
+
+popupModal.querySelector(".popup-modal__close").addEventListener("click", () => {
+  popupModal.classList.remove("is--visible");
+  bodyBlackout.classList.remove("is-blacked-out");
+});
+
+bodyBlackout.addEventListener("click", () => {
+  popupModal.classList.remove("is--visible");
+  bodyBlackout.classList.remove("is-blacked-out");
+});
