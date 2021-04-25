573d67c8fd384da30f22697859ac7af7e7951e84
leeinsong <insong1216@gmail.com>
2021-04-25 20:24:41 +0900

    Add: Digital Clock

--- 2nd_seminar/index.js
+++ 2nd_seminar/index.js
@@ -1,8 +1,9 @@
 const secondhand = document.querySelector(".second-hand");
 const minhand = document.querySelector(".min-hand");
 const hourhand = document.querySelector(".hour-hand");
+const clockTarget = document.querySelector(".digitalClock");
 
-function setDate() {
+function setAnalog() {
   const now = new Date();
 
   const seconds = now.getSeconds();
@@ -18,5 +19,28 @@ function setDate() {
   hourhand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
-setDate();
+function setDigital() {
+  let date = new Date();
+  let month = date.getMonth();
+  let clockDate = date.getDate();
+  let day = date.getDay();
+
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  clockTarget.innerText =
+    `${month + 1}월 ${clockDate}일 ${week[day]}요일` +
+    `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+      seconds < 10 ? `0${seconds}` : seconds
+    }`;
+}
+
+function init() {
+  setDigital();
+  setAnalog();
+  setInterval(setDigital, 1000);
+  setInterval(setAnalog, 1000);
+}
+
+init();
