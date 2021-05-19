716eab1a723f5c8fb65076633f1758f489ca593c
leeinsong <insong1216@gmail.com>
2021-05-19 16:16:15 +0900

    [init] step4 : Header, Title, Footer 더하기

--- 4th_homework/diary/src/App.js
+++ 4th_homework/diary/src/App.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainHeader from "./components/common/MainHeader";
+import Title from "./components/common/Title";
+import Footer from "./components/common/Footer";
+
+const getCurrentDate = () => {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth();
+  return { year: currentYear, month: currentMonth };
+};
+
 function App() {
-  return <div>Hello Diary App</div>;
+  const [year, setYear] = useState(getCurrentDate().year);
+  const [month, setMonth] = useState(getCurrentDate().month);
+  return (
+    <>
+      <Router>
+        <MainHeader />
+        <Title />
+      </Router>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
